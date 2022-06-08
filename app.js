bef402250193f57bedfa7620a4c58a3d1fba7663
anacsff <anacsfonseca@outlook.com.br>
2022-06-07 21:44:21 -0300

    Aula 07/06

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 const userRoute = require("./src/routes/userRoute");
 
+//rodar a aplicação; USAR tal parametro no app, nesse caso, configurar um pacote de rotas, pacote de usuário.
+//são chamadas  através do postman, para funcionar devem ser vinculadas ao app
+app.use(express.json());
 app.use(userRoute);
 
 app.listen(port, () => {
