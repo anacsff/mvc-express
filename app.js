dd807adfda208a68791723e8e6bd582ceeaac60b
anacsff <anacsfonseca@outlook.com.br>
2022-06-09 21:54:58 -0300

    Aula 09/06 EJS

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const userRoute = require("./src/routes/userRoute");
+const indexRoute = require("./src/routes/indexRoute")
 
-//rodar a aplicação; USAR tal parametro no app, nesse caso, configurar um pacote de rotas, pacote de usuário.
+//Configura o conteudo estático para acesso externo
+app.use(express.static(__dirname + "/public"));
+//Configura o template engine
+app.set("view engine", "ejs");
+//Configura o caminho para os views
+app.set("views", __dirname + "/src/views");
 //são chamadas  através do postman, para funcionar devem ser vinculadas ao app
 app.use(express.json());
-app.use(userRoute);
+app.use(indexRoute);
+//rodar a aplicação; USAR tal parametro no app, nesse caso, configurar um pacote de rotas, pacote de usuário.
+app.use("/user", userRoute);
 
 app.listen(port, () => {
   console.log("Estamos rodando na porta: " + port);
